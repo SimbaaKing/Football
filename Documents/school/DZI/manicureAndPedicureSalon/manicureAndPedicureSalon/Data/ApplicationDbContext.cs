9ecce494509286ec20d126f6efced1a86965eb42
SimbaaKing <bozikovagabriela400@gmail.com>
2022-03-16 13:17:11 +0200

    First

---
+++ Documents/school/DZI/manicureAndPedicureSalon/manicureAndPedicureSalon/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace manicureAndPedicureSalon.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Client>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Appoitment> Appoitments { get; set; }
+        public DbSet<Service> Services { get; set; }
+
+    }
+}
